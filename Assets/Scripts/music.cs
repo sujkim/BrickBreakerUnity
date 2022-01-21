a42de13ca99beeb38150375ccf08098efe02c99d
sujkim <13511934+sujkim@users.noreply.github.com>
2022-01-20 20:09:09 -0500

    removed files

--- Assets/Scripts/music.cs
+++ Assets/Scripts/music.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class music : MonoBehaviour
+public class Music : MonoBehaviour
 {
     public AudioSource bgMusic;
     // Start is called before the first frame update
